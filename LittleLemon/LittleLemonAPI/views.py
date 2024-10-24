634428cfdb0003be4fcaeff6949ca47d6f0e26ef
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-23 21:41:37 -0700

    Create ModelViewSet class

--- LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemonAPI/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import MenuItem
-from .serializers import MenuItemSerializers
+from .serializers import MenuItemSerializer
+
 # Create your views here.
+
+class MenuItemsViewSet(viewsets.ModelViewSet):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
