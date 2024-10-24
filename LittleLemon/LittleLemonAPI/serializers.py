3c816bc43541a1aeae25fb4c8d61539efd047db8
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-23 21:39:15 -0700

    CreateCreate serializers

---
+++ LittleLemon/LittleLemonAPI/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from .models import MenuItem
+import bleach
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    def validate_title(self, value):
+        return bleach.clean(value)
+    class Meta:
+        model = MenuItem
+        fields = [
+            'id', 'title', 'price', 'inventory'
+        ]
+        extra_kwargs = {
+            'price':{'min_value': 2},
+            'inventory':{'min_value': 0}
+        }
